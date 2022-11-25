d912cbdf11c8c9489c02fbfa7f9be9e3e700be41
simrankaurgithub <simr35478@gmail.com>
2022-11-25 18:26:19 +0530

    first commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 
+class StudentSerializer(serializers.Serializer):
+    name = models.CharField(max_length = 100)
+    roll = models.IntegerField()
+    city = models.CharField(max_length = 100)
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
 
 # Create your views here.
+def test_api(request):
+    stu=Student.objects.all()
+    serializer = StudentSerializer(stu)
+    
